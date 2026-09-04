e7cf3aa8b2359b8233aae757375ec6a6062d33ab
Prajwal Gamotech <digitalbuddiesspune@gmail.com>
2026-09-04 22:21:57 +0530

    chutiya

--- launchService/services/gameRepository.js
+++ launchService/services/gameRepository.js
@@ -13,17 +13,3 @@ export async function fetchGameById(gameId) {
 
   return data.game;
 }
-
-export async function fetchGameByCode(gameCode) {
-  const { data } = await axios.get(GAMES_BASE_URL);
-
-  if (!data?.success || !Array.isArray(data.games)) {
-    throw new Error("Invalid response from games API");
-  }
-
-  return (
-    data.games.find(
-      (game) => game.code === gameCode || game.slug === gameCode
-    ) ?? null
-  );
-}
